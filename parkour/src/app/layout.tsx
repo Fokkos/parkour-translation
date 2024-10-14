ce0b78e25b321f6533ff0086a893412361f2d43d
James March <54935498+Fokkos@users.noreply.github.com>
2024-10-14 21:14:51 +0100

    vercel please don't charge me £1000

--- parkour/src/app/layout.tsx
+++ parkour/src/app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Analytics />
       </body>
     </html>
   );
